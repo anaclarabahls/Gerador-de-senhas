5938e8c8bc64c2f7a66f56f7127f2684efea8e34
anaclarabahls <160857705+anaclarabahls@users.noreply.github.com>
2025-06-06 07:33:19 -0300

    .

---
+++ main.js
@@ -0,0 +1,8 @@
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+let tamanhoSenha = 5;
+numeroSenha.textContent = tamanhoSenha;
+
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+let tamanhoSenha = 5;
+numeroSenha.textContent = tamanhoSenha;
+
