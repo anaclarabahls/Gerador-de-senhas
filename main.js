69589851c619faefbe4313bb44dc1b81bc3e3ad5
anaclarabahls <160857705+anaclarabahls@users.noreply.github.com>
2025-06-27 08:26:33 -0300

    .

--- main.js
+++ main.js
@@ -75,8 +75,7 @@ forcaSenha.classList.add('forte');
 }
 else if (tamanhoSenha > 5 && tamanhoSenha < 12 ){
 forcaSenha.classList.add('media');
-else if (tamanhoSenha <= 5){
-}
+} else if (tamanhoSenha <= 5){
 
 forcaSenha.classList.add('fraca');
 }
