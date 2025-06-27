32052f93ed1df3bbc3aa7e7e4648cfcbf84f03f1
anaclarabahls <160857705+anaclarabahls@users.noreply.github.com>
2025-06-27 08:22:55 -0300

    .

--- main.js
+++ main.js
@@ -75,7 +75,9 @@ forcaSenha.classList.add('forte');
 }
 else if (tamanhoSenha > 5 && tamanhoSenha < 12 ){
 forcaSenha.classList.add('media');
+else if (tamanhoSenha <= 5){
 }
 
 forcaSenha.classList.add('forte');
+}
 }
